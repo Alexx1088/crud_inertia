5a2536dde3eba916b4ea36f123a549ed797a5c70
Alexx108 <alexx1088@gmail.com>
2022-10-17 22:46:01 +0600

    add delete route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return inertia('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -25,7 +25,15 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return inertia('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('post.index');
+Route::get('/posts/create', [\App\Http\Controllers\PostController::class, 'create'])->name('post.create');
+Route::get('/posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show');
+Route::post('/posts', [\App\Http\Controllers\PostController::class, 'store'])->name('post.store');
+Route::get('/posts/{post}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('post.edit');
+Route::patch('/posts/{post}', [\App\Http\Controllers\PostController::class, 'update'])->name('post.update');
+Route::delete('/posts/{post}', [\App\Http\Controllers\PostController::class, 'delete'])->name('post.delete');
 
 require __DIR__.'/auth.php';
